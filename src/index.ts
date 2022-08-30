f3809010da54f6fb927f43d194a1607277d3ddb7
denisbindyukov <gitgroom@gmail.com>
2022-08-30 22:00:43 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express'
 const app = express()
-const port = process.env.PORT || 5001
+const port = process.env.BLA || 5001
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello BRO!')
