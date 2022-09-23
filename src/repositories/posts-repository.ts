f50b2b6bd4056e14409db9c4dccfc9839ea5b5ae
denisbindyukov <gitgroom@gmail.com>
2022-09-23 14:39:56 +0300

    Blogs/Posts

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -1,4 +1,5 @@
-import {defaultBlogId, defaultBlogName} from "./blogs-repository";
+import {blogRepository, defaultBlogId, defaultBlogName} from "./blogs-repository";
+import {PostDtoType} from "../routs/posts-router";
 
 export let posts = [
     {
@@ -12,6 +13,60 @@ export let posts = [
 ]
 
 export const postsRepository = {
+    getPosts() {
+        return posts
+    },
+
+    createPost(dto: PostDtoType) {
+        const blog = blogRepository.getBlogById(dto.blogId)
+
+        if (blog) {
+            const newPost = {
+                id: new Date().toISOString(),
+                title: dto.title,
+                shortDescription: dto.shortDescription,
+                content: dto.content,
+                blogId: dto.blogId,
+                blogName: blog.name
+            }
+            posts.push(newPost);
+            return newPost;
+        } else {
+            return false
+        }
+
+    },
+    getPost(postId: string) {
+        const post = posts.find((el) => el.id === postId);
+
+        if (post) {
+            return post;
+        } else {
+            return false
+        }
+    },
+    updatePost(id: string, dto: PostDtoType) {
+        const post = posts.find((el) => el.id === id);
+
+        if (post) {
+            posts = posts.map(el => el.id === id ? {...el, ...dto} : el);
+            return true
+        } else {
+            return false
+        }
+
+    },
+
+    deletePost(id: string) {
+        const post = posts.find(el => el.id === id)
+
+        if (post) {
+            posts = posts.filter(el => el.id !== id)
+            return true
+        } else {
+            return false
+        }
+    },
 
     deleteAllItems() {
         while (posts.length > 0) {
--- src/routs/posts-router.ts
+++ src/routs/posts-router.ts
@@ -1,186 +1,66 @@
 import {Request, Response, Router} from "express";
-
-const todayDay = new Date().toISOString()
+import {postsRepository} from "../repositories/posts-repository";
 
 export const postsRouter = Router({});
 
-
-export let videos = [
-    {
-        id: 0,
-        title: "Redux",
-        author: "Dinis",
-        canBeDownloaded: false,
-        minAgeRestriction: 18 as null | number,
-        createdAt: todayDay,
-        publicationDate: todayDay,
-        availableResolutions: ['P144']
-    }
-];
-
-interface ReqBodyType {
-    title: string
-    author: string
-    canBeDownloaded: any
-    minAgeRestriction: number
-    publicationDate: any
-    availableResolutions: string[]
+export interface PostDtoType {
+    title: string,
+    shortDescription: string,
+    content: string,
+    blogId: string
 }
 
 postsRouter.get('/', (req: Request, res: Response) => {
-    res.status(200).send(videos)
+    const posts = postsRepository.getPosts()
+    res.status(200).send(posts)
 });
 
 postsRouter.get('/:id', (req: Request, res: Response) => {
-    const videoId = req.params.id;
-    const video = videos.find((v) => v.id === +videoId);
+    const post = postsRepository.getPost(req.params.id)
 
-    if (!video) {
-        res.status(404).send()
+    if (post) {
+        res.status(200).send(post)
         return
-    }
-
-    res.status(200).send(video)
-});
-
-
-postsRouter.post('/', (req: Request<{}, {}, ReqBodyType>, res: Response) => {
-    const {title, author, canBeDownloaded, availableResolutions, minAgeRestriction} = req.body;
-    let errors: any = {
-        errorsMessages: []
-    }
-
-    if (!title || title.trim().length === 0 || title.trim().length > 40) {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "title"
-        })
-    }
-
-    if (!author.trim()|| author.length > 20) {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "author"
-        })
-    }
-
-    if (typeof canBeDownloaded === 'boolean') {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "canBeDownloaded"
-        })
-    }
-
-    if (minAgeRestriction > 18) {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "minAgeRestriction"
-        })
-    }
-
-
-
-
-    if (errors.errorsMessages.length) {
-        res.status(400).send(errors)
+    } else {
+        res.status(404).send()
         return;
     }
 
 
-    function addDays(date: Date, days: number) {
-        const result = new Date(date);
-        result.setDate(result.getDate() + days);
-        return result;
-    }
-
-
-    const date = new Date();
-
-    const newVideos = {
-        id: +new Date().getTime(),
-        title,
-        author,
-        canBeDownloaded: false,
-        minAgeRestriction: null,
-        createdAt: date.toISOString(),
-        publicationDate: addDays(date, 1).toISOString(),
-        availableResolutions: ['P144']
-    }
-
-    videos.push(newVideos)
-
-    res.status(201).send(newVideos)
 });
 
-postsRouter.put('/:id', (req: Request<{ id: string }, {}, ReqBodyType>, res: Response) => {
-    const videoId = req.params.id;
-    const {title, author, canBeDownloaded, minAgeRestriction, publicationDate, availableResolutions} = req.body
-    const video = videos.find((v) => v.id === +videoId);
-    let errors: any = {
-        errorsMessages: []
-    }
+postsRouter.post('/', (req: Request<{}, {}, PostDtoType>, res: Response) => {
+    const post = postsRepository.createPost(req.body)
 
-    if (!video) {
+    if (post) {
+        res.status(201).send(post)
+    } else {
         res.status(404).send()
+    }
+
+});
+
+postsRouter.put('/:id', (req: Request<{ id: string }, {}, PostDtoType>, res: Response) => {
+    const postUpdated = postsRepository.updatePost(req.params.id, req.body)
+
+    if (postUpdated) {
+        res.status(204).send()
         return
-    }
-
-    if (!title || !title.trim() || title.length > 40) {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "title"
-        })
-    }
-
-    if (!author || !author.trim() || author.length > 20) {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "author"
-        })
-    }
-
-    if (typeof canBeDownloaded !== 'boolean') {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "canBeDownloaded"
-        })
-    }
-
-    if (minAgeRestriction > 18) {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "minAgeRestriction"
-        })
-    }
-
-    if (typeof publicationDate !== "string") {
-        errors.errorsMessages.push({
-            message: 'some error',
-            field: "publicationDate"
-        })
-    }
-
-    if (errors.errorsMessages.length) {
-        res.status(400).send(errors)
+    } else {
+        res.status(404).send()
         return;
     }
 
-    videos = videos.map((v) => v.id === +videoId ? {...v, ...req.body} : v);
-
-    res.status(204).send()
 });
 
 
 postsRouter.delete('/:id', (req: Request<{ id: string }>, res: Response) => {
-    const videoId = req.params.id;
-    const video = videos.find((v) => v.id === +videoId);
-    if (!video) {
-        res.status(404).send()
+    const postDeleted = postsRepository.deletePost(req.params.id)
+
+    if (postDeleted) {
+        res.status(204).send()
         return
+    } else {
+        res.status(404).send()
     }
-
-    const index = videos.findIndex((v) => v.id === +videoId);
-    videos.splice(index, 1)
-
-    res.status(204).send()
 });
